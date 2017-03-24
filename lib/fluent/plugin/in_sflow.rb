139e9ca189f5b118d4c943eb868730312f5ae2df
enukane <enukane@glenda9.org>
2017-03-24 14:41:55 +0900

    use fluent's server helper
    Signed-off-by: enukane <enukane@glenda9.org>

--- lib/fluent/plugin/in_sflow.rb
+++ lib/fluent/plugin/in_sflow.rb
@@ -13,37 +13,10 @@ end
 #require 'sflow'
 
 module Fluent::Plugin
-  module DatagramHandler
-    def initialize args = {}
-      @callbacks = args[:callbacks]
-    end
-
-    def receive_data(data)
-      sflow = SflowParser.parse_packet(data)
-      @callbacks.each do |callback|
-        callback.call sflow
-      end
-    end
-  end
-
-  class Collector
-    def initialize args = {}
-      @host = args[:host] || "127.0.0.1"
-      @port = (args[:port] || 6343).to_i
-
-      @callbacks = []
-
-      EventMachine::open_datagram_socket(@host, @port, DatagramHandler,
-                                        :callbacks => @callbacks)
-    end
-
-    def on_sflow &proc
-      @callbacks << proc
-    end
-  end
-
   class SflowInput < Input
     Fluent::Plugin.register_input("sflow", self)
+
+    helpers :server
 
     config_param :bind, :string, default: '0.0.0.0'
     config_param :port, :integer, default: 6343
@@ -52,33 +25,21 @@ module Fluent::Plugin
     def configure(conf)
       super
 
-      @th_evrun = nil
-      @stop_th = false
+      # dummy data
+      $switch_hash = {}
     end
 
     def start
       super
 
-      @th_evrun = Thread.new(&method(:run))
-    end
-
-    def run
-      EM::run do
-        c = Collector.new({:host => @host, @port => @port})
-        c.on_sflow do |sflow|
-          router.emit(@tag, Engine.now, sflow)
-        end
+      server_create(:in_sflow_server, @port, bind: @bind, proto: :udp, max_bytes: 2048) do |data, sock|
+        sflow = SflowParser.parse_packet(data)
+        router.emit(@tag, Fluent::EventTime.now, sflow)
       end
     end
 
     def shutdown
       super
-
-      if @th_evrun and @th_evrun.alive?
-        @stop_th = true
-        @th_evrun.join
-        @stop_th = false
-      end
     end
   end
 end
